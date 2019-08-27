e830e08a2132bffc0adca4fc2443876998424242
sayonsom <sayon@ieee.org>
2019-08-27 12:07:02 -0400

    bs4 is now working

--- main.py
+++ main.py
@@ -1,67 +1,36 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+
     
-class HTMLTableParser:
+class HTML_table_to_dataframe:
    
-    def parse_url(self, url):
+    def scrape(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, features="html.parser")
+        # soup = BeautifulSoup(open(url), "html.parser")
         return soup.find("table",{"class":"t4 simpletable2"})  
 
-    def parse_html_table(self, table):
+    def make_df(self, table):
         n_columns = 0
         n_rows=0
         column_names = []
 
-        # Find number of rows and columns
-        # we also find the column titles if we can
-        for row in table.find_all('tr'):
-            
-            # Determine the number of rows in the table
-            td_tags = row.find_all('td')
-            if len(td_tags) > 0:
-                n_rows+=1
-                if n_columns == 0:
-                    # Set the number of columns for our table
-                    n_columns = len(td_tags)
-                    
-            # Handle column names if we find them
-            th_tags = row.find_all('th') 
-            if len(th_tags) > 0 and len(column_names) == 0:
-                for th in th_tags:
-                    column_names.append(th.get_text())
+        df = pd.DataFrame({'Region' : [], 'Spot Price': [], 'Spark Spread': []})
 
-        # Safeguard on Column Titles
-        if len(column_names) > 0 and len(column_names) != n_columns:
-            raise Exception("Column titles do not match the number of columns")
-
-        columns = column_names if len(column_names) > 0 else range(0,n_columns)
-        df = pd.DataFrame(columns = columns,
-                          index= range(0,n_rows))
-        row_marker = 0
         for row in table.find_all('tr'):
-            column_marker = 0
-            columns = row.find_all('td')
-            for column in columns:
-                df.iat[row_marker,column_marker] = column.get_text()
-                column_marker += 1
-            if len(columns) > 0:
-                row_marker += 1
-                
-        # Convert to float if possible
-        for col in df:
-            try:
-                df[col] = df[col].astype(float)
-            except ValueError:
-                pass
-        
+            cols = row.find_all('td') # Data type = List of bs4.element.tag 
+            cols = [c.string for c in cols]
+            if len(cols) == 6:
+                 df = df.append(pd.Series([cols[0], cols[3], cols[5]], index=['Region','Spot Price','Spark Spread']), ignore_index=True)
         return df
 
 
+
 url = "https://www.eia.gov/todayinenergy/prices.php"
-hp = HTMLTableParser()
-table = hp.parse_url(url)
-x = hp.parse_html_table(table)
+# url = "C:\\Users\\CHANDS3\\Desktop\\eia.html"
+t = HTML_table_to_dataframe()
+table = t.scrape(url)
+x = hp.make_df(table)
 print(x)
 
